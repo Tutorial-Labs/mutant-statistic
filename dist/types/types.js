d146248d9b330673e488fdee6ab0ffdc4ed7ca7e
CristianDallosADL <Cristian.Dallos@avaldigitallabs.com>
2021-07-11 23:55:10 -0500

    :sparkles: Mutant: Remove node modules

--- dist/types/types.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.APPLICATION = void 0;
-exports.APPLICATION = {
-    MutantDatabaseService: Symbol.for("MutantDatabaseService.ts"),
-};
-//# sourceMappingURL=types.js.map
